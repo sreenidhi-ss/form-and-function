f8287f5316ef1f35ec071551f7895cd7374f01cb
sreenidhi-ss <sento.sreenidhi@gmail.com>
2025-11-25 10:05:49 -0500

    project6 and project7

---
+++ Project7-toggle/main.js
@@ -0,0 +1,22 @@
+
+
+	let togglebutton = document.getElementById('togglebutton');
+	let body = document.body;
+	let blueChair = document.getElementById('bluechair');
+	let orangeChair = document.getElementById('orangechair');
+
+	
+
+	togglebutton.addEventListener('click', function () {
+		
+		
+
+		
+		body.classList.toggle('orange');
+        
+        
+		
+        
+
+		
+	});
